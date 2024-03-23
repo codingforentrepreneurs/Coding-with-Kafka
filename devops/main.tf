326800fdd419c090f5e8601b1c4f9af8b2201350
CodingForEntrepreneurs <hello@teamcfe.com>
2024-03-23 12:02:52 -0600

    23 - Terraform Zookeeper and Kafka Instances

--- devops/main.tf
+++ devops/main.tf
@@ -9,3 +9,48 @@ terraform {
 provider "linode" {
   token = var.linode_api_token
 }
+
+resource "linode_instance" "zookeeper-instance" {
+    label = "zookeeper${count.index + 1}" # unique in linode
+    count = var.zookeeper_server_count
+    image = "linode/ubuntu22.04"
+    region = "us-sea"
+    type = var.server_type
+    authorized_keys = [trimspace(file(var.ssh_public_key_path))]
+    private_ip = true
+
+    provisioner "remote-exec" {
+        connection {
+            host = "${self.ip_address}"
+            type = "ssh"
+            user = "root"
+            private_key = "${file(var.ssh_private_key_path)}"
+        }
+        inline = [
+            "hostnamectl set-hostname zookeeper${count.index + 1}",
+        ]
+    }
+
+}
+
+resource "linode_instance" "kafka-instance" {
+    label = "kafka${count.index + 1}"
+    count = var.kafka_server_count
+    image = "linode/ubuntu22.04"
+    region = "us-sea"
+    type = var.server_type
+    authorized_keys = [trimspace(file(var.ssh_public_key_path))]
+    private_ip = true
+
+    provisioner "remote-exec" {
+        connection {
+            host = "${self.ip_address}"
+            type = "ssh"
+            user = "root"
+            private_key = "${file(var.ssh_private_key_path)}"
+        }
+        inline = [
+            "hostnamectl set-hostname kafka${count.index + 1}",
+        ]
+    }
+}
--- devops/variables.tf
+++ devops/variables.tf
@@ -1,3 +1,31 @@
 variable "linode_api_token" {
   sensitive = true
 }
+
+variable "server_type" {
+    default = "g6-standard-2"
+    type = string
+}
+
+variable "zookeeper_server_count" {
+    default = 1
+    type = number
+}
+
+variable "kafka_server_count" {
+    default = 1
+    type = number
+}
+
+variable "ssh_public_key_path" {
+    description = "SSH public key path to use for Linode instances"
+    default     = "~/.ssh/id_rsa.pub"
+    type        = string
+    sensitive   = false
+}
+
+variable "ssh_private_key_path" {
+    description = "Path to the SSH public key"
+    default = "~/.ssh/id_rsa"
+    sensitive   = true
+}
