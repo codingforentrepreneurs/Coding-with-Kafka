8f853620a1a79a2bda2557bb6163f20ad252f457
CodingForEntrepreneurs <hello@teamcfe.com>
2024-03-27 11:56:58 -0600

    37 - FastAPI + Kafka & AIOKafkaProducer

--- src/consume.py
+++ src/consume.py
@@ -1,16 +1,29 @@
 import json
 from kafka import KafkaConsumer, TopicPartition
-
+import pathlib
+from decouple import Config, RepositoryEnv
 # KAFKA_BROKER_URL="127.0.0.1:19092"
 # KAFKA_BROKER_URL2="127.0.0.1:19093"
 # KAFKA_BROKER_URL3="127.0.0.1:19094"
 # bootstrap_servers=[KAFKA_BROKER_URL, KAFKA_BROKER_URL2, KAFKA_BROKER_URL3]
 
-KAFKA_BROKER_URL="172.234.228.67:19092"
-bootstrap_servers=[KAFKA_BROKER_URL]
+# KAFKA_BROKER_URL="172.234.228.67:19092"
+# bootstrap_servers=[KAFKA_BROKER_URL]
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+CLUSTER_ENV_PATH = BASE_DIR / "cluster.env"
+config = Config(RepositoryEnv(str(CLUSTER_ENV_PATH)))
+
+KAFKA_BROKER_1=config('KAFKA_BROKER_1', default=None)
+KAFKA_BROKER_2 = config("KAFKA_BROKER_2", default=None)
+KAFKA_BROKER_3 = config("KAFKA_BROKER_3", default=None)
+KAFKA_BROKER_4 = config("KAFKA_BROKER_4", default=None)
+KAFKA_BROKER_5 = config("KAFKA_BROKER_5", default=None)
+bootstrap_servers = [KAFKA_BROKER_1, KAFKA_BROKER_2, KAFKA_BROKER_3, KAFKA_BROKER_4, KAFKA_BROKER_5]
 
 
-KAFKA_TOPIC="my-new-topic"
+
+KAFKA_TOPIC="some_topic"
 
 consumer = KafkaConsumer(
     KAFKA_TOPIC,
--- webapp_fastapi/main.py
+++ webapp_fastapi/main.py
@@ -1,17 +1,54 @@
 import pathlib
 from typing import Union
+from contextlib import asynccontextmanager
+import json
 
+from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI
+from decouple import Config, RepositoryEnv
 
-app = FastAPI()
 
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+CLUSTER_ENV_PATH = BASE_DIR / "cluster.env"
+config = Config(RepositoryEnv(str(CLUSTER_ENV_PATH)))
+
+KAFKA_BROKER_1=config('KAFKA_BROKER_1', default=None)
+KAFKA_BROKER_2 = config("KAFKA_BROKER_2", default=None)
+KAFKA_BROKER_3 = config("KAFKA_BROKER_3", default=None)
+KAFKA_BROKER_4 = config("KAFKA_BROKER_4", default=None)
+KAFKA_BROKER_5 = config("KAFKA_BROKER_5", default=None)
+bootstrap_servers = [KAFKA_BROKER_1, KAFKA_BROKER_2, KAFKA_BROKER_3, KAFKA_BROKER_4, KAFKA_BROKER_5]
+
+workers = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    producer = AIOKafkaProducer(bootstrap_servers=bootstrap_servers)
+    await producer.start()
+    workers['producer'] = producer
+    yield 
+    try:
+        await producer.stop()
+    except:
+        pass
+    workers.clear()
+
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
-def read_root(order_id:str=None):
+async def read_root(order_id:str=None):
     print(order_id)
+    event_data = {"type": "orders/start", "order_id": order_id}
+    data = json.dumps(event_data).encode("utf-8")
+    producer = workers.get("producer")
+    if producer is not None:
+        topic = "some_topic"
+        await producer.send_and_wait(topic, data)
     return {"Hello": "World", "BASE_DIR": BASE_DIR}
 
 
