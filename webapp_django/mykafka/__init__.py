a21ef829a98f9cd4d0659c66ca4148921c92c7bd
Justin Mitchel <jmitchel3@gmail.com>
2024-04-11 11:49:11 -0600

    Chapter 2 updates

--- webapp_django/mykafka/__init__.py
+++
@@ -1,7 +0,0 @@
-from .client import get_consumer, get_producer, send_topic_data
-
-__all__ = [
-    'get_consumer', 
-    'get_producer', 
-    'send_topic_data'
-]
--- webapp_fastapi/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-uvicorn main:app --reload
